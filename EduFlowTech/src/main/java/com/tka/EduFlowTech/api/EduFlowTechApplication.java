a2c3fd11c27c4cec03d166ef0c6c04e6ef07e380
shraddhakalbande <shraddhakalbande10@gmail.com>
2025-02-07 17:40:35 +0530

    Add files via upload

---
+++ EduFlowTech/src/main/java/com/tka/EduFlowTech/api/EduFlowTechApplication.java
@@ -0,0 +1,14 @@
+package com.tka.EduFlowTech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduFlowTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduFlowTechApplication.class, args);
+		System.err.println("App started");
+	}
+
+}
---
+++ EduFlowTech/src/main/java/com/tka/EduFlowTech/api/exceptions/DivideByZeroException.java
@@ -0,0 +1,8 @@
+package com.tka.EduFlowTech.api.exceptions;
+
+public class DivideByZeroException extends RuntimeException{
+
+	public DivideByZeroException(String msg) {
+		super(msg);
+	}
+}
